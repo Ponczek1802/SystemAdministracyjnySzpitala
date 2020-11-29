cb086047c37062c09f6faea01b9651d887ed6479
Ponczek1802 <61319943+Ponczek1802@users.noreply.github.com>
2020-11-29 18:11:20 +0100

    upload

---
+++ Struktura/Program.cs
@@ -0,0 +1,19 @@
+﻿using Struktura.Klasa;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Struktura
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pracownik pracownik = new Lekarz(Enum.Specjalizacja.Neurolog, 123, "dsad", "asdas", 1, "da", "dsad");
+            
+            Console.ReadKey();
+        }
+    }
+}
